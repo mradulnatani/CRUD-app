79aa04a54aaab681964dde2e1a00c663addf6714
mradul_09 <mradulnatani0@gmail.com>
2024-05-09 00:01:06 +0530

    added css to Todos.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,9 @@ import './App.css';
 import Header from "./MyComponents/Header.js";
 import Todos from "./MyComponents/Todos.js";
 import Footer from "./MyComponents/Footer.js";
-function App() {
-  /*
-  let curdItems =[ {
-    sno : 1,
-    title : "First demo statement",
-    desc : "Description about the first demo statement"
-},
-{
-  sno : 2,
-  title : "Second demo statement",
-  desc : "Description about the second demo statement"
-},
-{
-    sno : 3,
-    title : "Third demo statement",
-    desc : "Description about the third demo statement"
-},
-]
-*/  return (
+function App() { 
+
+  return (
    <>
   <Header title= "CURD-Application" searchBar = {true}/>
   <Todos title="Curd-Operations"/>
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,27 +1,17 @@
 import React from 'react'
-
+import '../MyComponents/Todos.css'
 export default function Todos(porps) {
-  const userData={
-    email_id : document.querySelector("#exampleFormControlInput1").innerHTML,
-    Data : document.querySelector("#exampleFormControlTextarea1").innerHTML,
-}
-let btnclick = document.querySelector(".create");
-    btnclick.addEventListener(onclick,()=>{
-    document.getElementById("output").innerHTML = userData.email_id;
-    document.getElementById("output2").innerHTML = userData.Data;
-
-})
   return (
     <>
-    <h1>About....</h1>
-    <span>       A CRUD app is a specific type of software application that revolves around four fundamental operations:
+    <h1 className='AboutHeading'>About....</h1>
+    <p className='Discription'>       A CRUD app is a specific type of software application that revolves around four fundamental operations:
 
 Create: This operation allows you to add new data to your application. For instance, in a library management app, you can create a new book record.
 Read: The read operation lets you retrieve data from your database. It doesn't alter data; it simply displays it. You can query your database to fetch all records (e.g., all books in the library) or specific records based on criteria (e.g., vegan recipes).
 
 Update: With the update operation, you can modify existing data. For example, you might update a book's details (e.g., change the author or update the publication year).
 
-Delete: The delete operation allows you to remove data from your database. If you want to remove a book from your library, this operation handles it.             </span>
+Delete: The delete operation allows you to remove data from your database. If you want to remove a book from your library, this operation handles it.             </p>
 
 <hr/>
 <hr/>
