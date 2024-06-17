92d2321fd369f9de2a82815f75e0918f815f00e9
mradul_09 <mradulnatani0@gmail.com>
2024-06-17 16:16:40 +0530

    merged components into one

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import Header from './Header.js';
 import Main from './Main.js';
-import DeleteData from './deleteData.js';
-import ReadData from './readData.js'
 function App() { 
     return(
         <>
         <Header/>
         <Main/>
-        <DeleteData/>
-        <ReadData/>
+      
     </>
     );
 }
--- src/Main.js
+++ src/Main.js
@@ -14,6 +14,13 @@ export default function Main() {
             <p>{data}</p>
             :null
         }
+        <br/> <br/>
+              Enter id to read the data: <input type='text' style={{margin:20, height:20, width:400, padding:0}}></input>
+              <button style={{width:200}}>Read</button>
+              <br/> <br/>
+              Enter id of data to be deleted: <input type='text' style={{margin:20, height:20, width:400, padding:0}}></input>
+              <button style={{backgroundColor:'red',width:200}}>Delete</button>
+
         </>
     )
 }
