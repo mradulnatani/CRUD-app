7c3ef3278d0f0820b35324e5d4833574ea759ecb
mradul_09 <mradulnatani0@gmail.com>
2024-07-01 18:02:43 +0530

    changed ui

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
     <>
       <nav className="navbarbg-body-tertiary" style={{backgroundColor:'black',color:'white'}}>
   <div className="container-fluid">
-    <span className="navbar-text">
+    <span className="navbar-text" style={{fontSize:'30px'}}>
       Welcome to our CRUD Application...
     </span>
   </div>
--- src/Main.js
+++ src/Main.js
@@ -29,14 +29,17 @@ export default function Main() {
 
   return (
     <>
-      <form>
-       <input placeholder='Enter Data' style={{height:'100px',width:'500px',borderRadius:'5px',marginTop:'100px',marginLeft:'500px'}}type="text" value={data} onChange={settingData} />
-        <br /><br />
-        <button onClick={showData} style={{height:'50px',width:'100px',borderRadius:'5px',marginLeft:'700px',backgroundColor:'blue',color:'white'}} type="button">Save</button>
+      <form style={{border:'1px solid blue',borderRadius:'1px',backgroundColor:'red'}}>
+        <h1 style={{marginTop:'170px',color:'white'}}>Enter data:</h1>
+       <input placeholder='Enter Data' style={{height:'100px',width:'500px',borderRadius:'5px', marginTop:'10px',marginLeft:'100px'}}type="text" value={data} onChange={settingData} />
+        <br />
+        <button onClick={showData} style={{height:'40px',width:'100px',borderRadius:'5px',marginLeft:'500px',backgroundColor:'blue',color:'white'}} type="button">Save</button>
       </form>
+      <br/>
+      <h3>Output:</h3>
       {show ? <h1 style={{marginLeft:'700px'}}>{data}</h1> : <h1>No Data</h1>}
       {taskArray.map((task, index) => (
-        <h3 key={index}>{task}<button onClick={()=>{deleteTask(index)}} style={{height:'50px',width:'100px',borderRadius:'5px',marginLeft:'700px',backgroundColor:'red',color:'white'}}>Delete</button><button style={{height:'50px',width:'100px',borderRadius:'5px',marginLeft:'2px',backgroundColor:'blue',color:'white'}} onClick={() => editTask(index, prompt('Enter updated task:'))}>Edit</button>
+        <h3 key={index}>{task}<button onClick={()=>{deleteTask(index)}} style={{height:'40px',width:'100px',borderRadius:'5px',marginLeft:'10px',backgroundColor:'red',color:'white'}}>Delete</button><button style={{height:'40px',width:'100px',borderRadius:'5px',marginLeft:'2px',backgroundColor:'blue',color:'white'}} onClick={() => editTask(index, prompt('Enter updated task:'))}>Edit</button>
         <hr/>
 </h3>
       ))}
